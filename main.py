36eb379a51d9921449e7160026699cefe43a96af
elixc20 <elicombs2020@gmail.com>
2024-02-08 18:40:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    if word == word[::-1]:
+        return ('True')
+    else:
+        return ('False')
 
 if __name__ == '__main__': 
-    #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    word = input()
+    print(palindrome(word))
